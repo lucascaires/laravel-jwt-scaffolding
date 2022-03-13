929635ad3fae40bf46568e61e4a6feb7fe5fbb07
Lucas Caires <lucas.cairesd@gmail.com>
2022-03-13 16:12:13 -0400

    change from phpunit to pest

--- tests/Feature/JwtAuthTest.php
+++ tests/Feature/JwtAuthTest.php
@@ -1,67 +1,37 @@
 <?php
 
-namespace Tests\Feature;
-
 use App\Models\User;
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class JwtAuthTest extends TestCase
-{
-    use RefreshDatabase;
+uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
 
-    public function testGuestCannotSeeAProtectedRoute()
-    {
-        $response = $this->json('GET', 'api/me');        
-        $response->assertStatus(401); //unauthorized status code
-    }
+test('if a guest will not see a protected route')
+    ->get('api/me')
+    ->assertStatus(401);
 
-    public function testGuestsCanRegister()
-    {
-        $response = $this->json('POST', 'api/register', [
-            'name' => 'example',
-            'email' => 'example@example.com',
-            'password' => 'example',
-            'password_confirmation' => 'example'
-        ]);   
+test('if guests can register')
+    ->post('api/register', [
+        'name' => 'example',
+        'email' => 'example@example.com',
+        'password' => 'example',
+        'password_confirmation' => 'example'
+    ])->assertJsonStructure([
+        'access_token', 'token_type', 'expires_in'
+    ])->assertStatus(200);
 
+test('if a not registered user cannot loggin')
+    ->post('api/login', [
+        'email' => 'random@random.com',
+        'password' => 'password'
+    ])->assertJsonStructure([
+        'error'
+    ])->assertStatus(401);
 
-
-        $response->assertJsonStructure([
-            'access_token', 'token_type', 'expires_in'
-        ]);
-        $response->assertStatus(200);
-    }
-
-    public function testIfNotRegisteredUserCanLogIn()
-    {
-        $user = User::factory()->make();
-        
-        $response = $this->json('POST', 'api/login', [
-            'email' => $user->email,
-            'password' => 'password'
-        ]);   
-
-        $response->assertJsonStructure([
-            'error'
-        ]);
-
-        $response->assertStatus(401); //Unauthorized
-    }
-
-    public function testUsersCanLoggedIn()
-    {
-        $user = User::factory()->create();
-        
-        $response = $this->json('POST', 'api/login', [
-            'email' => $user->email,
-            'password' => 'password'
-        ]);   
-
-        $response->assertJsonStructure([
-            'access_token', 'token_type', 'expires_in'
-        ]);
-        $response->assertStatus(200);
-    }
-}
+test('if a user can login', function () {
+    $user = User::factory()->create();
+    $this->json('POST', 'api/login', [
+        'email' => $user->email,
+        'password' => 'password'
+    ])->assertJsonStructure([
+        'access_token', 'token_type', 'expires_in'
+    ])->assertStatus(200);
+});
